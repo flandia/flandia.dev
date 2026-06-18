bca2642c517ebe1fe8bb1527489cc09267c21d8d
Flandia Yingman <me@flandia.dev>
2026-06-18 11:49:23 +0800

    feat(awards): add Reaching Out Award
    Add Reaching Out Award (HKSAR Government Scholarship Fund, 2025-26) to the Awards & Achievements section.

--- src/data/Body.tsx
+++ src/data/Body.tsx
@@ -259,6 +259,11 @@ const Body = () => (
         <EntryTime>2025-26</EntryTime>
       </Entry>
       <Entry>
+        <EntryTitle>Reaching Out Award</EntryTitle>
+        <EntrySubtitle>HKSAR Government Scholarship Fund</EntrySubtitle>
+        <EntryTime>2025-26</EntryTime>
+      </Entry>
+      <Entry>
         <EntryTitle>IET Prize 2025</EntryTitle>
         <EntrySubtitle>IET Hong Kong</EntrySubtitle>
         <EntryTime>2025-26</EntryTime>
